5669feffbe24865cd51fa6985e7db684bbd47144
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2023-11-13 19:08:39 +0800

    fix: preserve line breaks in echo

--- delta-prod-docker-entrypoint.sh
+++ delta-prod-docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo $DELTA_SHARING_SERVER_YAML > ./conf/delta-sharing-server.yaml
-echo $CORE_SITE_XML > ./conf/core-site.xml
+echo "$DELTA_SHARING_SERVER_YAML" > ./conf/delta-sharing-server.yaml
+echo "$CORE_SITE_XML" > ./conf/core-site.xml
 
 ./bin/delta-sharing-server -- --config ./conf/delta-sharing-server.yaml
