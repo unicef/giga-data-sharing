fdb27e47f7200b31f4afb073d3a8113e583424ce
tm-ger-chagas <122899250+tm-ger-chagas@users.noreply.github.com>
2023-11-29 19:35:25 +0800

    docs: [US49] swagger doc annotations (#7)
    * docs: add annotation descriptions
    
    * docs: add annotations to routes
    
    * docs: add missing annotations to routes

--- data_sharing/annotations/delta_sharing.py
+++ data_sharing/annotations/delta_sharing.py
@@ -1,3 +1,25 @@
+delta_sharing_capabilities_header_description = (
+    "Indicates the format to expect. Two Values are supported:"
+    " `responseFormat:parquet` Represents the format of the delta sharing protocol"
+    " that has been used in delta-sharing-spark 1.0 and less, and is also the default"
+    " format if `responseFormat` is missing from the header. `responseFormat:delta` "
+    " format can be used to read a shared delta table with minReaderVersion > 1, which"
+    " contains readerFeatures such as Deletion Vector or Column Mapping."
+    "`readerfeatures` is only useful when responseformat=delta"
+)
+
+ending_version_description = "The ending version of the query, inclusive."
+
+ending_timestamp_description = (
+    "The ending timestamp of the query, a string in the Timestamp Format, which will be"
+    " converted to a version created less than or equal to this timestamp."
+)
+
+include_historical_metadata_description = (
+    "If set to true, return the historical metadata if seen in the delta log. This is"
+    " for the streaming client to check if the table schema is still read compatible."
+)
+
 max_results_description = (
     "The maximum number of results per page that should be returned. If the number of"
     " available results is larger than `maxResults`, the response will provide a"
@@ -14,4 +36,16 @@ page_token_description = (
     " be returned in a response if there are no more results available."
 )
 
+
+schema_name_description = "The schema name to query. It's case-insensitive."
+
 share_name_description = "The share name to query. It's case-insensitive."
+
+starting_timestamp_description = (
+    "The starting timestamp of the query, a string in the Timestamp Format, which will"
+    " be converted to a version created greater or equal to this timestamp."
+)
+
+starting_version_description = "The starting version of the query, inclusive."
+
+table_name_description = "The table name to query. It's case-insensitive."
