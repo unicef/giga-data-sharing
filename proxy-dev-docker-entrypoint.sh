59d5e6683fcac3eedee2e936c26eeafbb071563c
Brian Musisi <brianmusisi@berkeley.edu>
2025-12-02 10:31:32 +0100

    chore: add schemas to load_fixtures list

--- proxy-dev-docker-entrypoint.sh
+++ proxy-dev-docker-entrypoint.sh
@@ -4,6 +4,6 @@ set -eu pipefail
 
 poetry install --no-root --with dev
 poetry run alembic upgrade head
-poetry run python -m scripts.load_fixtures roles
+poetry run python -m scripts.load_fixtures roles schemas
 
 exec poetry run uvicorn main:app --host 0.0.0.0 --port 5000 --reload
